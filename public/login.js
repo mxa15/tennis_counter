27f0d98d32b54545440c4154d9732580a770c30e
mxa15 <gurschlermax@gmail.com>
2026-08-06 19:10:43 +0200

    Update

--- public/login.js
+++ public/login.js
@@ -2,7 +2,9 @@ function login() {
   const name = document.getElementById("name");
   const password = document.getElementById("password");
   const subtitle = document.getElementById("subtitle");
+  const loadin_screen = document.getElementById("loading");
 
+  loadin_screen.style.display = "flex";
   fetch("/api/login", {
     method: "POST",
     headers: {
@@ -17,6 +19,7 @@ function login() {
     .then((data) => {
       const status = data.status;
 
+      loadin_screen.style.display = "none";
       if (status == "ok") {
         location.href = "/startseite";
       } else if (status == "incorect password") {
