7a7016a87c3782974647fe73a269ff25ca08eb55
mxa15 <gurschlermax@gmail.com>
2026-08-24 18:29:38 +0200

    Update

--- server.js
+++ server.js
@@ -792,7 +792,7 @@ wss.on("connection", (socket) => {
   });
 });
 
-function updateviewers() {}
+function updateviewers(matchsettings) {}
 
 server.listen(PORT, () => {
   console.log("server läuft auf port:" + PORT);
