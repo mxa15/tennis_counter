2d25b0e5131b5a496d05d4c01395783c7f1ef147
mxa15 <gurschlermax@gmail.com>
2026-08-24 21:45:23 +0200

    Update

--- public/view_match.js
+++ public/view_match.js
@@ -235,16 +235,3 @@ ${c2}<br>`;
   p.innerHTML = output;
   window.scrollTo(0, document.body.scrollHeight);
 }
-
-async function shareWebsite() {
-  console.log("guuut");
-  try {
-    await navigator.share({
-      title: "Meine Tennis-Website",
-      text: "Schau dir meine Tennis-Website an!",
-      url: window.location.href,
-    });
-  } catch (err) {
-    console.log("Teilen abgebrochen");
-  }
-}
