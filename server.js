8b4cfb43a3dc1b66a29c7a1dc832bee6a67ccd5b
mxa15 <gurschlermax@gmail.com>
2026-08-25 12:11:23 +0200

    Update

--- server.js
+++ server.js
@@ -782,11 +782,6 @@ wss.on("connection", (socket) => {
         data.matchcode,
       ]);
 
-      if (match.rows.length === 0) {
-        socket.send(JSON.stringify({ type: "noMatch" }));
-        return;
-      }
-
       socket.send(
         JSON.stringify({
           type: "getmatchData",
