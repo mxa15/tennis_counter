724eae10703aa56ff00898a57e7f6d592892eb02
mxa15 <gurschlermax@gmail.com>
2026-08-25 17:11:18 +0200

    Update

--- public/match.js
+++ public/match.js
@@ -81,7 +81,7 @@ async function getdata() {
     finish_match();
   }
 
-  const setToWin = matchsettings.code.max_sets === 3 ? 2 : 3;
+  const setToWin = matchsettings.code.max_sets == 3 ? 2 : 3;
   info.innerHTML = `
 matchcode: ${matchsettings.code}<br>
 ein Satz geht bis ${matchsettings.data.set}<br>
