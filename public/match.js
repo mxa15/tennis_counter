02a1c4ae527e75fb8eda831c11630611732d6fdd
mxa15 <gurschlermax@gmail.com>
2026-08-25 18:18:18 +0200

    Update

--- public/match.js
+++ public/match.js
@@ -444,8 +444,8 @@ async function shareMatch() {
   try {
     await navigator.share({
       title: "Live Tennis Spiel",
-      text: "Schau dir meine Tennis-Website an!",
-      url: viewURL,
+      text: `schau dir das Tennis Spiel zwischen ${matchsettings.data.player1} und ${matchsettings.data.player2} live an.
+klicke auf diesen Link ➡️${viewURL}`,
     });
   } catch (err) {
     console.log("Teilen abgebrochen");
