96a828f5fb070b7693d17e1720d3e8082780725d
mxa15 <gurschlermax@gmail.com>
2026-08-26 09:33:00 +0200

    Update

--- public/main.js
+++ public/main.js
@@ -116,7 +116,7 @@ async function get_friends() {
     add_friendelement(friend.username, friend.id);
     friendids.push(friend.id);
   });
-  addmatches(friendids, "livematches", "created");
+  addmatches(friendids, "livematches", "live");
 }
 
 get_friends();
