ee6842d7a8c8250190c1284994a8f55c25505024
mxa15 <gurschlermax@gmail.com>
2026-08-26 09:33:30 +0200

    Update

--- public/main.js
+++ public/main.js
@@ -238,7 +238,7 @@ function addmatches(id, tableid, status) {
   });
 }
 
-addmatches(["my_id"], "mymatches", "created");
+addmatches(["my_id"], "mymatches", "all");
 
 function getDate(dateString) {
   const timeZone = "Europe/Rome";
