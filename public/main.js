926f620c0272690d1a8ae82d0065b83b48d3b549
mxa15 <gurschlermax@gmail.com>
2026-08-31 15:47:23 +0200

    Update

--- public/main.js
+++ public/main.js
@@ -488,6 +488,7 @@ function openPopUp(text, color = "gray", time = 1500) {
   }, time + 100);
 }
 
+openPopUp("hallo zuhause", "red", 6000);
 setTimeout(() => {
   openPopUp("ortung fehlgeschlagen", "red", 2000);
 }, 5000);
