c059c5094ad5f3f677bf5d426a9689cc968e76e7
mxa15 <gurschlermax@gmail.com>
2026-09-04 18:09:47 +0200

    Update

--- public/match.js
+++ public/match.js
@@ -230,6 +230,9 @@ function update_tabelle(point) {
     (matchsettings.points.sets[matchsettings.points.sets.length - 1][0] == 4 &&
       matchsettings.points.sets[matchsettings.points.sets.length - 1][1] == 4 &&
       matchsettings.data.set == 4) ||
+    (matchsettings.points.sets[matchsettings.points.sets.length - 1][0] == 4 &&
+      matchsettings.points.sets[matchsettings.points.sets.length - 1][1] == 4 &&
+      matchsettings.data.set == "3-3") ||
     matchsettings.points.championstiebrake == true
   ) {
     tabele.game[0].innerHTML = matchsettings.points.tiebrake[0];
