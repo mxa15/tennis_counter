d07e04318df044619f88c3a33ef96701c6315a49
mxa15 <gurschlermax@gmail.com>
2026-09-04 14:49:55 +0200

    Update

--- public/main.js
+++ public/main.js
@@ -708,11 +708,6 @@ async function getfavoritNames() {
 }
 
 async function deletefavoritName(name) {
-  const element = document.getElementById("name-" + name);
-  element.classList.add("delete_friend");
-  setTimeout(() => {
-    element.remove();
-  }, 1000);
   const response = await fetch("/api/SQL", {
     method: "POST",
     headers: {
