476e48c9fa6d2739f9fe4faf7041b1a21065ea37
mxa15 <gurschlermax@gmail.com>
2026-09-05 19:21:15 +0200

    Update

--- public/main.js
+++ public/main.js
@@ -419,9 +419,8 @@ async function start_match() {
   let position;
   let posdata;
 
-  console.log(document.getElementById("aloowposition").checked);
-
   if (document.getElementById("aloowposition").value == "OFF") {
+    document.getElementById("aloowposition").checked;
   }
 
   try {
