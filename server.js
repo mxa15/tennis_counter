a21d4002bd534a6af2e298ea99b45444663ca96b
mxa15 <gurschlermax@gmail.com>
2026-09-06 10:23:31 +0200

    Update

--- server.js
+++ server.js
@@ -34,6 +34,7 @@ app.get("/", (req, res) => {
 
 app.get("/startseite", (req, res) => {
   res.sendFile(path.join(__dirname, "html_files", "index.html"));
+  console.log(req.headers["user-agent"]);
 });
 
 app.get("/login", (req, res) => {
