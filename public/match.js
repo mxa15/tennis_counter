245b09d7920950eaaf00be3f03e187ede395b3de
mxa15 <gurschlermax@gmail.com>
2026-09-11 22:32:22 +0200

    Update

--- public/match.js
+++ public/match.js
@@ -493,9 +493,6 @@ function loadTheme() {
   if (cookie) {
     const theme = cookie.split("=")[1];
     document.documentElement.setAttribute("data-theme", theme);
-    if (theme == "dark") {
-      darkmode.checked = true;
-    }
   }
 }
 
