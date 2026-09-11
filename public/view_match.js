4fc6bde6c6c213160c92fdec8a847104c3854f0f
mxa15 <gurschlermax@gmail.com>
2026-09-11 22:34:33 +0200

    Update

--- public/view_match.js
+++ public/view_match.js
@@ -351,3 +351,16 @@ function openPopUp(text, color = "gray", time = 1500) {
     }, 500);
   }, time + 100);
 }
+
+function loadTheme() {
+  const cookie = document.cookie
+    .split("; ")
+    .find((row) => row.startsWith("theme="));
+
+  if (cookie) {
+    const theme = cookie.split("=")[1];
+    document.documentElement.setAttribute("data-theme", theme);
+  }
+}
+
+loadTheme();
