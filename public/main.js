5d595496609a2ff488da6f3a48bea11fe0eebe2f
mxa15 <gurschlermax@gmail.com>
2026-09-13 09:52:44 +0200

    Update

--- public/main.js
+++ public/main.js
@@ -469,6 +469,8 @@ async function start_match() {
     ? JSON.parse(localStorage.getItem("tournament"))
     : [];
 
+  localStorage.setItem("tournaments", JSON.stringify(tourn.push(tournament)));
+
   location.href = "/match/" + code.code;
 }
 
