ccac3b3dffcadf0aa7ff66e4f74a274d25c41c4f
mxa15 <gurschlermax@gmail.com>
2026-09-16 13:51:08 +0200

    Update

--- public/main.js
+++ public/main.js
@@ -822,8 +822,8 @@ async function search_match(search) {
     body: search,
   });
   const data = await response.json();
+  const output = document.getElementById("matchSearchResults");
   if (data.status == "ok") {
-    const output = document.getElementById("matchSearchResults");
     output.innerHTML = `<p class="big-text">suchergebnisse für "${search}"`;
     data.matches.forEach((match) => {
       let url = match.my
@@ -874,5 +874,7 @@ async function search_match(search) {
           </div>
         </div>`;
     });
+  } else {
+    output.innerHTML = `<p class="big-text">es wurde keine übereinstimung mit "${search}" gefunden`;
   }
 }
