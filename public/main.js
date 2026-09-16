db56f4b9adc74f66c77f02398ebb4ddc1a32c400
mxa15 <gurschlermax@gmail.com>
2026-09-16 13:52:29 +0200

    Update

--- public/main.js
+++ public/main.js
@@ -875,6 +875,6 @@ async function search_match(search) {
         </div>`;
     });
   } else {
-    output.innerHTML = `<p class="big-text">keine übereinstimung mit ${search}`;
+    output.innerHTML = `<p class="big-text">keine übereinstimung mit "${search}"`;
   }
 }
