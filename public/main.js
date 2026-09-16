51da69e33b8c863c47cd16b281cfb366cb198add
mxa15 <gurschlermax@gmail.com>
2026-09-16 13:28:28 +0200

    Update

--- public/main.js
+++ public/main.js
@@ -206,8 +206,6 @@ async function addmatches(id, tableid, status) {
     return;
   }
   matches.forEach((match) => {
-    console.log(match);
-
     const div = document.createElement("div");
     div.classList.add("matches");
     let servers = ["", ""];
@@ -828,6 +826,7 @@ async function search_match(search) {
     const output = document.getElementById("matchSearchResults");
     output.innerHTML = "";
     data.matches.forEach((match) => {
+      console.log(match);
       let game = [0, 0];
       if (match.points.tiebrake[0] > 0 || match.points.tiebrake[1] > 0) {
         game = [match.points.tiebrake[0], match.points.tiebrake[1]];
